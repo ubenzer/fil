8988d4f780e64229a3c310d55376d0de5ba904ab
Umut Benzer <umutbenzer@gmail.com>
2017-08-27 22:23:46 +0200

    Fix cache enabled/disabled detection

--- app/contentManager.js
+++ app/contentManager.js
@@ -21,7 +21,7 @@ export class ContentManager {
 
   async metaOf({id, type}) {
     const handler = this._getHandlerFor({type})
-    const cacheEnabledCalculatorFn = handler.useChildrenCache || handler.childrenWatcher
+    const cacheEnabledCalculatorFn = handler.useChildrenCache || (() => Boolean(handler.childrenWatcher))
     const useCache = cacheEnabledCalculatorFn({id, type})
 
     const valueFn = async () => {
@@ -54,7 +54,7 @@ export class ContentManager {
 
   async valueOf({id, type}) {
     const handler = this._getHandlerFor({type})
-    const cacheEnabledCalculatorFn = handler.useContentCache || handler.contentWatcher
+    const cacheEnabledCalculatorFn = handler.useContentCache || (() => Boolean(handler.contentWatcher))
     const useCache = cacheEnabledCalculatorFn({id, type})
 
     const valueFn = async () =>
